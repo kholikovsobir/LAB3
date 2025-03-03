29db96060f0b7fa4825f430f7633d006200d316c
kholikovsobir <sobirholikov63@gmail.com>
2025-03-04 03:17:11 +0700

    Add files via upload

---
+++ LAB3/script.js
@@ -0,0 +1,4 @@
+// Обратный звонок
+document.getElementById('callback-btn').addEventListener('click', function() {
+    alert('Обратный звонок запрошен! Мы свяжемся с вами в ближайшее время.');
+});
